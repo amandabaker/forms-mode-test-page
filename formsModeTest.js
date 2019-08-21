5a6991f1d7fd336546c2548ec9a4603e8eb74dfe
Amanda Baker <amandaabaker21@gmail.com>
2019-08-21 15:09:22 -0700

    Add another list

--- formsModeTest.js
+++ formsModeTest.js
@@ -1,17 +1,17 @@
 
 // Helpers
 const onInvokeButton = (element, callback) => {
-    element.addEventListener('click', callback);
-    element.addEventListener('keydown', (event) => {
-        if (event.key === ' ' || event.key === 'Enter') {
-            callback(event);
-            event.preventDefault();
-        }
-    });
+  element.addEventListener('click', callback);
+  element.addEventListener('keydown', (event) => {
+    if (event.key === ' ' || event.key === 'Enter') {
+      callback(event);
+      event.preventDefault();
+    }
+  });
 }
 
 const setRole = (element, role) => {
-    element.setAttribute('role', role);
+  element.setAttribute('role', role);
 };
 
 
@@ -19,47 +19,54 @@ const setRole = (element, role) => {
 
 // Button
 const setupButton = () => {
-    const button = document.getElementById('button-div');
-    button.textContent = 'Test';
-    const toggleButton = () => {
-        if (button.textContent === 'Test') {
-            button.textContent = 'Untest';
-        } else {
-            button.textContent = 'Test';
-        }
-    };
-    onInvokeButton(button, toggleButton);
+  const button = document.getElementById('button-div');
+  button.textContent = 'Test';
+  const toggleButton = () => {
+    if (button.textContent === 'Test') {
+      button.textContent = 'Untest';
+    } else {
+      button.textContent = 'Test';
+    }
+  };
+  onInvokeButton(button, toggleButton);
 };
 setupButton();
 
 
 // Document Structure Roles
-const setupList = () => {
-    const list = document.getElementById('list-div');
-    for (let i = 0; i < 5; i++) {
-        const listItem = document.createElement('div');
-        listItem.tabIndex = -1;
-        setRole(listItem, 'listitem');
-        listItem.textContent = `List item ${i}`;
-        listItem.addEventListener('keydown', (event) => {
-            if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
-                const offset = event.key === 'ArrowUp' ? -1 : 1;
-                const newIndex = list.index + offset;
-                if (newIndex < 0 || newIndex >= list.children.length)
-                    return;
+const setupList = (id) => {
+  const list = document.getElementById(id);
+  const listItems = Array.from(list.children);
+  listItems.forEach((listItem, index) => {
+    listItem.tabIndex = -1;
+    listItem.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        const offset = event.key === 'ArrowUp' ? -1 : 1;
+        const newIndex = list.index + offset;
+        if (newIndex < 0 || newIndex >= list.children.length)
+          return;
 
-                const oldElement = list.children[list.index];
-                oldElement.tabIndex = -1;
-                const newElement = list.children[newIndex];
-                newElement.tabIndex = 0;
-                newElement.focus();
-                list.index = newIndex;
-                event.preventDefault();
-            }
-        });
-        list.appendChild(listItem);
-    }
-    list.index = 0;
-    list.children[0].tabIndex = list.index;
+        const oldElement = list.children[list.index];
+        oldElement.tabIndex = -1;
+        const newElement = list.children[newIndex];
+        newElement.tabIndex = 0;
+        newElement.focus();
+        list.index = newIndex;
+        event.preventDefault();
+      }
+    });
+    onInvokeButton(listItem, (event) => {
+      // unselect old, and select new
+      list.children[list.index].tabIndex = -1;
+      list.children[index].tabIndex = 0;
+      list.children[index].focus();
+      list.index = index;
+      event.preventDefault();
+    });
+  });
+  list.index = 0;
+  list.children[0].tabIndex = 0;
 }
-setupList();
+
+setupList('list-div');
+setupList('spans-list-div');
