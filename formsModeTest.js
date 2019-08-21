e45f9add9ef1fd3893909e55362a51592acd1c51
Amanda Baker <ambake@Amandas-MacBook-Pro.local>
2019-08-21 12:44:48 -0700

    Add button

--- formsModeTest.js
+++ formsModeTest.js
@@ -1,1 +1,29 @@
 
+// Helpers
+const onInvokeButton = (element, callback) => {
+    element.addEventListener('click', callback);
+    element.addEventListener('keydown', (event) => {
+        if (event.key === ' ' || event.key === 'Enter') {
+            callback(event);
+            event.preventDefault();
+        }
+    });
+}
+
+
+// Widgets
+
+// Button
+const button = document.getElementById('button-div');
+button.textContent = 'Test';
+const toggleButton = () => {
+    if (button.textContent === 'Test') {
+        button.textContent = 'Untest';
+    } else {
+        button.textContent = 'Test';
+    }
+};
+onInvokeButton(button, toggleButton);
+
+
+
